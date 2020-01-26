fb7e2a9dbbfd69c2c0304315252bc628c50c7336
Banan <ibaanan@gmail.com>
2020-01-26 20:00:41 +0100

    drobna poprawka

--- front/repertuar_add.php
+++ front/repertuar_add.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
+	
 	
 	// if (!isset($_SESSION['inicjuj']))
 	// {
@@ -11,7 +11,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
-<title>Zaloguj</title>
+<title>Dodaj repertuar</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="description" content="Place your description here" />
 <meta name="keywords" content="put, your, keyword, here" />
