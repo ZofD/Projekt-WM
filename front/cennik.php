c4fd5c54c15c343576c626c1648de34591308976
Banan <ibaanan@gmail.com>
2020-01-26 22:17:26 +0100

    mikro zmiana

--- front/cennik.php
+++ front/cennik.php
@@ -17,7 +17,7 @@ if($_SESSION['ip'] != $_SERVER['REMOTE_ADDR'])
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
-<title>Kontakt</title>
+<title>Cennik</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="description" content="Place your description here" />
 <meta name="keywords" content="put, your, keyword, here" />
