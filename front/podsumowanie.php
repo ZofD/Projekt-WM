9377469ae31dcde4eeb3adfa80004b633ec4a005
Banan <ibaanan@gmail.com>
2020-01-28 09:00:26 +0100

    zmiana w podsumowaniu

--- front/podsumowanie.php
+++ front/podsumowanie.php
@@ -146,20 +146,21 @@ $data = date("Y-m-d");
 			<div id="header">
 				<div class="row-1">
 				<div class="fleft"><a href="index.php">Kino<span>URZ</span></a></div>
-				<ul><li>Witaj <?php echo($_SESSION['login']); ?>, <?php if($_SESSION['zalogowany']){?><a style="text-decoration: none;" href="logout.php">wyloguj</a><?php } ?></li>
-				
+				<ul>
+					
+					<?php if($_SESSION['zalogowany']){ ?> <li>Witaj <?php echo($_SESSION['login']);  echo(", ");} if($_SESSION['zalogowany']){?><a style="text-decoration: none;" href="logout.php">wyloguj</a><?php } ?></li>
 					<li><a href="index.php"><img src="images/icon1-act.gif" alt="" /></a></li>
 						<li><a href="contact-us.php"><img src="images/icon2.gif" alt="" /></a></li>
-						<li><a href="Panel_Pracownika.php"><img src="images/icon3.gif" alt="" /></a></li>
-					</ul>
-				</div>
-				<div class="row-2">
-					<ul>
-						<li><a href="index.php" >Kino</a></li>
-						<li><a href="podsumowanie.php" class="active">Podsumowanie</a></li>
-						<li><a href="cennik.php" >Cennik</a></li>
-						<li><a href="contact-us.php" >Kontakt</a></li>
-						
+					
+						<!-- dodać tylko dla sesji pracownika i admina -->
+						<?php
+						if(isset($_SESSION['admin'])){
+							if($_SESSION['admin'] == 1 || $_SESSION['admin'] == 2){
+						?>
+							<li><a href="Panel_Pracownika.php"><img src="images/icon3.gif" alt="" /></a></li>
+						<?php
+						}}
+						?>
 					</ul>
 				</div>
 			</div>
